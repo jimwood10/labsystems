d2d4c3e52b9f5908c1dfb18177d76815619e9786
jimwood10 <86596995+jimwood10@users.noreply.github.com>
2024-05-22 15:05:40 +0100

    Update createFiles.sh

--- createFiles.sh
+++ createFiles.sh
@@ -4,15 +4,23 @@ echo "How many files do you want to add?"
 
 read NUMBER
 
+echo "What size do you want them to be? eg 10M, 10K, 10G etc"
+
+read SIZE
+
 echo "Where do you want to add them (full path)"
 
 read LOCATION
 
-
 for i in $(seq 1 $NUMBER)
 do
-touch $LOCATION/$i
+
+# touch $LOCATION/$i.
+
+fallocate -l $SIZE $LOCATION/$i.$SIZE.file
 
 done
 
-echo $NUMBER "files created in " $LOCATION
+# echo $NUMBER "files created in " $LOCATION
+
+echo $NUMBER "files of size " $SIZE " created in " $LOCATION
