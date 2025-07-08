75be9ebfc58f6619ae1af4affbad1ea6c8cddb8f
jimwood10 <86596995+jimwood10@users.noreply.github.com>
2025-07-08 11:23:32 +0100

    Update createFiles.sh

--- demoSystemFiles/createFiles.sh
+++ demoSystemFiles/createFiles.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
 
-echo "How many files do you want to add?"
-
-read NUMBER
-
-echo "What size do you want them to be? eg 10K, 10M, 10G etc"
-
-read SIZE
-
-echo "Where do you want to add them (full path)"
-
-read LOCATION
-
-echo "What prefix?"
-
-read PREFIX
-
-echo "What suffix?"
-
-read SUFFIX
-
+##### variables #####
+NUMBER=10
+SIZE=10K
+LOCATION=/gpfs/fs1
+PREFIX=test
+SUFFIX=mp3
+#
+# SIZE eg 10K, 10M, 10G etc
+##### variables #####
 
 for i in $(seq 1 $NUMBER)
 do
 
-# touch $LOCATION/$i.
-
 fallocate -l $SIZE $LOCATION/$PREFIX$i.$SUFFIX
 
 done
-
-# echo $NUMBER "files created in " $LOCATION
 
 echo $NUMBER "files of size " $SIZE " created in " $LOCATION
